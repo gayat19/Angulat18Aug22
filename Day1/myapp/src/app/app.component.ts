961d834a8c793664693d64e8a5fc8803818367af
Gayathri Mahadevan <gayat_19@yahoo.com>
2022-08-18 17:47:21 +0530

    Day1

---
+++ Day1/myapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',//element's name
+  templateUrl: './app.component.html',//html content
+  styleUrls: ['./app.component.css']//style content
+})
+export class AppComponent {
+  title = 'Gayathri Mahadevan';
+}
---
+++ Day1/myapp/src/app/first/first.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+customer_name:string;//variable creation
+chooseClass:string;
+toggleStyle:string;
+toggleValue:boolean;
+likesCount:number;
+
+  constructor() {
+    this.customer_name = "Tim";//Assignment of value
+    this.chooseClass = "mystyle";
+    this.toggleStyle = "glyphicon glyphicon-heart-empty";
+    this.toggleValue = false;
+    this.likesCount =0;
+   }
+
+  ngOnInit(): void {
+    console.log('on init of first component')
+  }
+
+  changeStyle(){
+    console.log("Event triggered");
+    this.chooseClass = "yourstyle";
+  }
+  toggle(){
+    if(this.toggleValue==false)
+    {
+      this.toggleValue=true;
+      this.toggleStyle = "glyphicon glyphicon-heart";
+    }
+    else
+    {
+      this.toggleValue=false;
+      this.toggleStyle = "glyphicon glyphicon-heart-empty";
+    }
+    
+  }
+  like(){
+    this.likesCount++;
+  }
+  changeName(custname:any){
+    this.customer_name=custname;
+  }
+}
